bfe30eca20d30443dd0a66f26a8c4fb25534880f
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-07 00:25:49 +0900

    first commit

---
+++ gitrmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitrmTest
+//
+//  Created by 에디 on 2023/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
